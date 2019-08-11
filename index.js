b72776a5387572ddd7372baa55a9f6f3abde6c46
Barak Saidoff <barak.saidoff@gmail.com>
2019-08-11 17:40:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ module.exports = { name, height
 }
 
 if (name === "Susan") {
-    console.log("The name is correct ")
+    console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan")
+}
+
+if (height === "74"){
+  console.log("The height is correct")
+
+} else {
+  console.log("The height is incorrect")
 }
